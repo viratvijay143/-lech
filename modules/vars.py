f989d9aed0b9da2361190a1ecbc9a89b6bbc8bc9
amitsha1 <121236130+amitsha1@users.noreply.github.com>
2023-12-01 17:12:09 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,9 +1,3 @@
-from os import getenv
-from dotenv import load_dotenv
-
-load_dotenv()
-
-API_ID = int(getenv("API_ID", 0))
-API_HASH = getenv("API_HASH", None)
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-
+api_id = 21359555
+api_hash = "526d12e4855af175e0e3d6e22beba6aa"
+bot_token = "6930088546:AAFOkh1wlahXpi-gPaN2GozXjSLo_nSHSYg"
