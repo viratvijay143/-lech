ec4b1d8e5e1fb9d37558e8f1819669392da65b0b
amitsha1 <121236130+amitsha1@users.noreply.github.com>
2023-12-01 17:10:45 +0530

    Add files via upload

---
+++ modules/vars.py
@@ -0,0 +1,9 @@
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(getenv("API_ID", 0))
+API_HASH = getenv("API_HASH", None)
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+
