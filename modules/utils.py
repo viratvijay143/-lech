2296f1ccd92bcba312d9d6a4dac1dbfd52936128
Rahulsinghcreator <138279223+Rahulsinghcreator@users.noreply.github.com>
2023-12-02 08:26:23 +0530

    Update utils.py

--- modules/utils.py
+++ modules/utils.py
@@ -66,6 +66,7 @@ def hrt(seconds, precision = 0):
 
 timer = Timer()
 
+# Powered By AIR PHEONIX
 async def progress_bar(current, total, reply, start):
     if timer.can_send():
         now = time.time()
@@ -91,7 +92,7 @@ async def progress_bar(current, total, reply, start):
             progress_bar = "▓" * completed_length + "▒" * remaining_length
             
             try:
-                await reply.edit(f'`\n ╭──⌯════Bot Statics═════⌯──╮ \n├⚡ {progress_bar}|﹝{perc}﹞ \n├🚀 Speed » {sp} \n├📟 Processed » {cur}\n├🧲 Size - ETA » {tot} - {eta} \n╰─═══ ✪Powered By META✪ ═══─╯`\n') 
+                await reply.edit(f'`\n ╭──⌯════Bot Statics═════⌯──╮ \n├⚡ {progress_bar}|﹝{perc}﹞ \n├🚀 Speed » {sp} \n├📟 Processed » {cur}\n├🧲 Size - ETA » {tot} - {eta} \n╰─═══ ✪ Powered By AIR PHEONIX ✪ ═══─╯`\n') 
             except FloodWait as e:
                 time.sleep(e.x)
 
