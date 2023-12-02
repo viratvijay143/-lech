793c3febe3b689e20c6a54c7be0ce97216c6f246
Rahulsinghcreator <138279223+Rahulsinghcreator@users.noreply.github.com>
2023-12-02 09:02:48 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = 
-api_hash = ""
-bot_token = "6930088546:AAFOkh1wlahXpi-gPaN2GozXjSLo_nSHSYg"
+api_id = 21408627
+api_hash = "465cca6fa782695983d49db306e0f8be"
+bot_token = "6579435839:AAHYst-WuDe_g116Fg0uoxY3NSa94R9asBI"
