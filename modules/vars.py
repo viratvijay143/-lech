93129d0aa4dd31a4ad5e11171cb0d63d76dd373c
kishansutahr15092005 <118811946+kishansutahr15092005@users.noreply.github.com>
2024-01-16 14:21:16 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "27862677"
 api_hash = "e343ce2c81b2b6c2c0d6bee58284e3bd"
-bot_token = "6777157028:AAFQGhd28KK0yjmCyP6WtmUTYuseHGm71Q0"
+bot_token = "6844287098:AAG5vghZHjwtdVSSDtK2owBg1CnZAmpQ8Go"
