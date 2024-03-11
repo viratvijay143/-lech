fbd5f813023dc785cbdaf46069e3cfa0752af10b
Aj4ybot <118820596+Aj4ybot@users.noreply.github.com>
2024-03-11 12:01:06 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "20047839"
 api_hash = "e635f85a4dae812a26c450c0d41276b0"
-bot_token = "6151489159:AAHpCVLq6thLVM_pTmC0ZoXPMuMwLDHL4_0"
+bot_token = "7076822577:AAGlE5moCHIHq_QmbPB8EZz1Kwz2RgqyJUU"
