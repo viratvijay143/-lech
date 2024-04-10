868913b5f5fb70ee1fdc9a128587fa7cc36e4531
SAHU <161798305+viratvijay143@users.noreply.github.com>
2024-04-10 14:35:58 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "20047839"
 api_hash = "e635f85a4dae812a26c450c0d41276b0"
-bot_token = "7150834185:AAFpgAEEZDDRGGiYJGYQVcxanPb2c2X5H9o"
+bot_token = "7005788592:AAEGdLelWp2DAoYA8MjGJRfvGA9darBQ0WI"
